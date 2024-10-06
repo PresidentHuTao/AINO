0f98c22152794f073106d63ad909625e9f5608a5
PresidentHuTao <pmhoang29084@gmail.com>
2024-10-07 05:40:10 +0700

    Initial commit

---
+++ src/main/java/com/example/aino/AinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AinoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aino/AinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
